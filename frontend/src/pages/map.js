36251b38fc116f4f25213f0105c4767ce8fb55d0
Shana <maryjop10@gmail.com>
2023-09-13 18:40:26 -0500

    Improving map style

--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -1,24 +1,84 @@
 import React from 'react';
-import '../styles/map.css';
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON, MarkerClusterGroup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
-
+import L from 'leaflet';
 
 const Map = () => {
-    return (
-        <div>
-            <h1>Opcion 1</h1>
-        <MapContainer center={[48.8566,2.3922]} zoom={2} minZoom={2} maxBounds={2}  className='MapContainer'>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-        </MapContainer>
+  const center = [45, 7]; // Latitud y longitud iniciales
+  const zoom = 2; // Nivel de zoom inicial
+  var arrowIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+    iconSize: [15, 25], // Tamaño de la flecha
+    iconAnchor: [10, 20], // Punto de anclaje de la flecha
+  });
 
-        <h1>Opcion 2</h1>
-        
-        </div>
-    );
-    
-}
+  const geojsonFeature = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          name: "Colombia",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [-74.081749, 4.609710], // Coordenadas de Colombia
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          name: "Alemania",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [10.451526, 51.165691], // Coordenadas de Alemania
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          name: "Japón",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [139.691710, 35.689487], // Coordenadas de Japón
+        },
+      },
+      // Puedes agregar más países aquí
+    ],
+  };
+
+  const onEachCountry = (feature, layer) => {
+    if (feature.properties && feature.properties.name) {
+      layer.bindPopup(feature.properties.name);
+      layer.on({
+        mouseover: () => {
+          layer.openPopup();
+        },
+        mouseout: () => {
+          layer.closePopup();
+        },
+      });
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', marginTop: '50px', marginBottom: '50px' }}>
+      <MapContainer
+        center={center}
+        minZoom={2}
+        zoom={zoom}
+        style={{ height: '680px', width: '1100px', margin: '20px', display: 'flex', padding: '20px'}}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoJSON data={geojsonFeature} pointToLayer={(feature, latlng) => L.marker(latlng, { icon: arrowIcon })} onEachFeature={onEachCountry} />
+      </MapContainer>
+    </div>
+  );
+};
+
 export default Map;
--- frontend/src/pages/map2.js
+++ frontend/src/pages/map2.js
@@ -1,126 +1,27 @@
-import "../styles/styles.css";
-import { VectorMap } from "react-jvectormap";
-import { useRef } from "react";
+import React from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Map2 = () => {
-    const mapRef = useRef();
-    return (
-      <div className="world">
-        <h1>Opción 2</h1>
-        <div style={{width: 500, height: 500}}>
-            <VectorMap ref={mapRef} map={"world_mill"}
-            />
-        </div>
+  const center = [45, 7]; // Latitud y longitud iniciales
+  const zoom = 2; // Nivel de zoom inicial
+  const handleClick = (e) => {
+    console.log(e.latlng) // muestra en consola las coordenadas
+  }
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      style={{ height: '500px', width: '100%' }}
+      onClick={handleClick}>
+      <TileLayer
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors'
+      />
+      <Marker position={[4.660779178475431, -74.08494168529754]}>
+        <Popup>This is your marker<br />Easily customizable.</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
-
-        {/*<div className="world-container">
-          <VectorMap
-            ref={mapRef}
-            // zoomOnScroll={false}
-            zoomButtons={false}
-            map={"world_mill"}
-            backgroundColor="transparent"
-            containerStyle={{
-              width: "100%",
-              height: "1000px"
-            }}
-            /*markerStyle={{
-              initial: {
-                fill: "#fff",
-                stroke: "#383f47"
-              }
-            }}*/
-            /*containerClassName="map"
-            series={{
-              //regions: [
-                {
-                  scale: ["#E2AEFF", "#5E32CA"],
-                  attribute: "fill",
-                  values: { SG: 100, IN: 100 },
-                  normalizeFunction: "polynomial",
-                  min: 0,
-                  max: 100
-                }
-              ]
-            }}
-            /* seri */
-            /*markers={[
-              {
-                //latLng: [1.3521, 103.8198],
-                //name: "Singapore"
-              }
-            ]}*/
-            /*regionStyle={{
-              initial: {
-                fill: "#D1D5DB",
-                "fill-opacity": 1,
-                stroke: "#265cff",
-                "stroke-width": 0,
-                "stroke-opacity": 0
-              },
-              hover: {
-                "fill-opacity": 0.8,
-                fill: "",
-                stroke: "#2b2b2b"
-              },
-              selected: {
-                fill: "#FFFB00"
-              }
-            }}
-            // onMarkerTipShow={function (event, label, index) {
-            //   label.html(
-            //     '<div style="background-color: white; border: 1px solid; min-height: 100px; max-width: 250px">' +
-            //       "<b style='color: red;'>" +
-            //       "Rsc" +
-            //       "</b><br/>" +
-            //       "<b>Population:</b>" +
-            //       "test" +
-            //       "</br>" +
-            //       "<b>Unemployment rate: </b>" +
-            //       "kay" +
-            //       "%" +
-            //       "</div>"
-            //   );
-            // }}
-            onRegionTipShow={function (event, label, code, ...props) {
-              console.log("-----> ", label.html(), event, label, code, props);
-              label.html(
-                '<div style="background-color: white; border: 1px solid white; outline: 10px solid white; border-radius: 5px; min-height: 70px; width: 150px; color: black"; padding-left: 10px>' +
-                  "<p>" +
-                  "<b>" +
-                  label.html() +
-                  "</b>" +
-                  "</p>" +
-                  "<p>" +
-                  "Count: " +
-                  "<b> Count</b>" +
-                  "</p>" +
-                  "</div>"
-              );
-            }}
-            // regions={[
-            //   {
-            //     scale: {
-            //       red: "#ff0000",
-            //       green: "#00ff00"
-            //     },
-            //     attribute: "fill",
-            //     values: {
-            //       "US-KS": "red",
-            //       "US-MO": "red",
-            //       "US-IA": "green",
-            //       "US-NE": "green"
-            //     },
-            //     legend: {
-            //       horizontal: true,
-            //       title: "Color"
-            //     }
-            //   }
-            // ]}
-          />
-        </div>*/}
-      </div>
-    );
-    
-}
 export default Map2;
