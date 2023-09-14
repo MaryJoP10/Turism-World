1565e9a04f25c43c46b3b1e09b9c4d9561aed41b
Shana <maryjop10@gmail.com>
2023-09-14 12:49:21 -0500

    Changing Navbar style (not finished)

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -2,26 +2,66 @@ import React, { useState } from "react";
 import "./navbar.css";
 import { NavLink as Link } from "react-router-dom";
 import styled from 'styled-components'
+import Logo from "../../images/logo.png"
+import { FiSettings } from "react-icons/fi"
+import { Container, InputAdornment, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleConfig, setToggleConfig] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+  const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#A0AAB4',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#B2BAC2',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#E0E3E7',
+      },
+      '&:hover fieldset': {
+        borderColor: '#B2BAC2',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#6F7E8C',
+      },
+    },
+  });
   return (
     <>
       <Nav className="Nav">
         <div className="logopages_container">
-          <div className="pages">
-            <NavLink  to="/">
-              <h1>Inicio</h1>
+          <div className="logo">
+            <NavLink to="/">
+              <img src={Logo} />
             </NavLink>
+          </div>
+          <div className="pages">
             <NavLink to="/map">
               <h1>Mapa</h1>
             </NavLink>
-            <NavLink to="/config">
-              <h1>Configuración</h1>
-            </NavLink>
-            <NavLink to="/help">
-              <h1>Ayuda</h1>
-            </NavLink>
+            <div className="search_container">
+                <TextField className="search_field"
+                  type="search"
+                  label="Buscar"
+                  value={searchInput}
+                  onChange={handleChange}
+                  sx={{ width: 300 }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <SearchIcon className="search_icon"/>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </div>
           </div>
           <div className="register">
             <NavLink to="/signin">
@@ -30,9 +70,25 @@ const Navbar = () => {
             <NavLink to="/signup">
               <button>Sign Up</button>
             </NavLink>
+            {toggleConfig
+              ? <FiSettings className="icon" onClick={() => setToggleConfig(false)} />
+              : <FiSettings className="icon" onClick={() => setToggleConfig(true)} />
+            }
+            {toggleConfig && (
+              <div className="config_menu">
+                <NavLink to="/config">
+                  <h1>Configuración</h1>
+                </NavLink>
+                <NavLink to="/help">
+                  <h1>Ayuda</h1>
+                </NavLink>
+              </div>
+            )}
           </div>
+
         </div>
         <div className="responsive">
+          {/*Responsive Hambar*/}
           {toggleMenu
             ? <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setToggleMenu(false)} width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -46,19 +102,11 @@ const Navbar = () => {
               <line x1="4" y1="18" x2="20" y2="18" />
             </svg>}
           {toggleMenu && (
-            <div className="responsive_container scale-up-center">
+            <div className="responsive_container">
               <div className="pagesResponsive">
-                <NavLink  to="/">
-                  <h1>Inicio</h1>
-                </NavLink>
-                <NavLink to="/mapa">
+
+                <NavLink to="/map">
                   <h1>Mapa</h1>
-                </NavLink>
-                <NavLink to="/config">
-                  <h1>Configuración</h1>
-                </NavLink>
-                <NavLink to="/help">
-                  <h1>Ayuda</h1>
                 </NavLink>
               </div>
               <div className="registerResponsive">
@@ -71,6 +119,23 @@ const Navbar = () => {
               </div>
             </div>
           )}
+          {/*Responsive Settings*/}
+          <div className="settings">
+            {toggleConfig
+              ? <FiSettings className="icon" onClick={() => setToggleConfig(false)} />
+              : <FiSettings className="icon" onClick={() => setToggleConfig(true)} />
+            }
+            {toggleConfig && (
+              <div className="config_menu">
+                <NavLink to="/config">
+                  <h1>Configuración</h1>
+                </NavLink>
+                <NavLink to="/help">
+                  <h1>Ayuda</h1>
+                </NavLink>
+              </div>
+            )}
+          </div>
         </div>
       </Nav>
     </>
