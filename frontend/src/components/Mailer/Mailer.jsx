964568fb01207ffc3123e3dd56677ec962cf5ea0
Shana <maryjop10@gmail.com>
2023-10-04 21:01:57 -0500

    Contact Us ready

--- frontend/src/components/Mailer/Mailer.jsx
+++ frontend/src/components/Mailer/Mailer.jsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import emailjs from '@emailjs/browser';
 import '../../styles/help.css';
 import './mailer.css';
 import Contact from '../../images/inicio.png';
+import Sent from '../../images/sent.gif';
 
 export const Mailer = () => {
-
+  const [isSent, setIsSent] = useState(false); //Estado de enviado
+  const navigate = useNavigate();
   const sendEmail = (event) => {
     event.preventDefault();
 
     emailjs.sendForm('service_i9g7lvk', 'template_3c6487f', event.target, 'Vgdhp2i-N0K0anawO')
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
+      .then((response) => {
+        console.log(response);
+        event.target.reset();
+        setIsSent(true);
+
+        const img = document.createElement('img');
+        img.src = Sent;
+        img.alt = 'Sent GIF';
+
+        img.style.display = 'flex';
+        img.style.justifyContent = 'center';
+        img.style.position = 'absolute';
+        img.style.marginTop = '10vh';
+        img.style.top = '50vh';
+        img.style.left = '50vw';
+        img.style.transform = 'translate(-50%, -50%)';
+
+        document.body.appendChild(img);
+
+        setTimeout(() => {
+          // Realiza la redirección suave con una transición de opacidad
+          const form = document.querySelector('.contact-form-wrapper');
+          form.style.transition = 'opacity 1s';
+          form.style.opacity = '0';
+
+          setTimeout(() => {
+            document.body.removeChild(img);
+            navigate('/');
+          }, 900);
+        }, 1000);
+      })
+
+
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
@@ -19,7 +56,7 @@ export const Mailer = () => {
       <section id="contact">
         <div class="contact-box">
           <div class="contact-links">
-             <img src={Contact}/>
+            <img alt="" src={Contact} />
           </div>
           <div class="contact-form-wrapper">
             <form onSubmit={sendEmail}>
